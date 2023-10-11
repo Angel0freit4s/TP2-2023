b7409efce8a16786f37573e14ae1a719d5b4883a
sushislow <eduardosf2307@gmail.com>
2023-10-11 12:02:10 -0300

    Atividade 22

---
+++ TP1-2023/README.md
@@ -0,0 +1,2 @@
+# TP2-2023
+Colagens criados em sala de TP1-2023
---
+++ TP1-2023/atividade.js
@@ -0,0 +1,22 @@
+const port = 8080;
+
+//Express
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/index", (request, response) => {
+response.render("index"); //views/index.ejs
+});
+
+app.get("/ola", (request, response) => {
+    response.send(`ola, ${request.query.nome} ${request.query.sobrenome}`); // views //
+    });
+
+app.use((request, response, next) => {
+response.status(404).send("<h1>Página não encontrada.</h1>");
+})
+
+app.listen(port, () => {
+console.log(`Servidor funcionando na porta: ${port}`);
+});
